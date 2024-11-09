29fd9ccf723a5734b7fa1e1421934e52bd7af4a9
ks2x00 <alamancali17@gmail.com>
2024-11-09 23:00:00 +0700

    feat: jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,30 @@ import { AppService } from './app.service';
 import { AppRepository } from './app.repository';
 import { PrismaModule } from './prisma/prisma.module';
 import { CustomerModule } from './customer/customer.module';
+import { AuthModule } from './auth/auth.module';
+import { AccessGuard } from './auth/guard/access-token.guard';
+import { APP_GUARD } from '@nestjs/core';
 
-const providers = [AppService, AppRepository];
+const providers = [
+  AppService,
+  AppRepository,
+  {
+    provide: APP_GUARD,
+    useClass: AccessGuard,
+  },
+];
 @Module({
-  imports: [PrismaModule, CustomerModule],
+  imports: [
+    PrismaModule,
+    CustomerModule,
+    AuthModule,
+    JwtModule.register({
+      global: true,
+      secret: 'rM0h6l5IrNeGnYXJ6qxbs3TRyVwqPc4oWQasTRyG',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AppController],
   providers: providers,
 })
-export class AppModule {}
+export class AppModule { }
