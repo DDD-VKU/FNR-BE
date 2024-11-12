be30198e150e7104b1ecaaa281a9015de2dc4ebc
ks2x00 <alamancali17@gmail.com>
2024-11-13 00:32:11 +0700

    feat: add auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AtStrategy, RtStrategy } from './strategies';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
 
 @Module({
-  providers: [RtStrategy, AtStrategy],
+  providers: [RtStrategy, AtStrategy, AuthService],
   controllers: [AuthController],
   imports: [JwtModule.register({})],
 })
